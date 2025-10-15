7b263d1b6803a5549f5ba67a4ca26ac704eef72a
YaoYao <you@example.com>
2025-10-15 23:01:15 +0800

    Abstract the "Game" class. 1. Abstract the "Game" class. 2. Abstract the game logic loop. 3. Game primarily consists of a logical world, World, and a rendering tool, Frame, responsible for rendering this logical world. When new objects are needed in the game world, they are added to the objs member variable. 4. When the game is paused, game logic updates should stop, but rendering should not. 5. Change the member variable of 'm_objs' to a pointer to take advantage of the polymorphic nature of Object

--- Snake/src/frame.cpp
+++ Snake/src/frame.cpp
@@ -3,67 +3,68 @@
 #include <vector>
 
 //一帧是由若干个图层组成的.不过按渲染顺序来. 逻辑和渲染分离.
-
 Frame::Frame(int H,  int W,  int OH,  int OW):
-    HEIGHT(H), WIDTH(W), OFFSET_H(OH), OFFSET_W(OW){
-    pixels=std::move(std::vector<std::vector<std::string>>(HEIGHT, std::vector<std::string>(WIDTH,std::string(" "))));
+    m_height(H), m_width(W), m_offset_h(OH), m_offset_w(OW){
+    m_pixels = std::move(std::vector<std::vector<std::string>>(m_height, std::vector<std::string>(m_width,std::string(" "))));
 }
 
+Frame::~Frame() {
+    // No dynamic memory to free, but if there were, it would be done here.
+}
 
 //show pixels on screen.
 bool Frame::flush_to_screen(World& world) {
-    auto& objs=world.get_objects();//copy or move?
-    for(auto&& obj : objs){//template delays type makes it's harder to find objs/obj typo.
-        auto& obj_body=obj.get_body();//no copy cost. Function 'get_body' with deduced return type cannot be used before it is defined, this is because of speration of declaration and definition.
-        auto& obj_outlook=obj.get_outlook();
-        int len = obj_body.size();
-        for(int i=0; i<len; i++){
-            auto [H,W]=obj_body[i];
+    auto& objs = world.get_objects();             //copy or move?
+    for(auto&& obj : objs){                     //template delays type makes it's harder to find objs/obj typo.
+        auto& obj_body = obj->get_body();        //no copy cost. Function 'get_body' with deduced return type cannot be used before it is defined, this is because of speration of declaration and definition.
+        auto& obj_outlook = obj->get_outlook();
+        for(int i = 0; i < obj_body.size(); i++){
+            auto [H,W] = obj_body[i];
             // std::printf("H: %d, W: %d, outlook: %s\n", H, W, (obj_outlook[i]).c_str());
-            mvaddstr(OFFSET_H + H, OFFSET_W + W, (obj_outlook[i]).c_str());
-            i++;
+            // 使用偏移量将对象放置在正确位置
+            mvaddstr(m_offset_h + H, m_offset_w + W, (obj_outlook[i]).c_str());
         }
     }
     return true;
 }
-    //detect if it is clash with other object
-    //only sometimes it will check. 
-    // template<typename Container>
-    // auto check_collision(Object it) -> decltype(std::forward<Container>(objs), int()) {
-    //     using ValueType = typename std::decay_t<Container>::value_type;
-    //     static_assert(
-    //         std::is_same_v<ValueType, std::shared_ptr<Object>>,
-    //         "Container must hold shared_ptr<Object>"
-    //     );
-    //     static_assert(
-    //         std::is_same_v<std::decay_t<Container>, std::vector<ValueType>>,
-    //         "Container must be a vector"
-    //     );
-    //     for(auto&& obj : objs){
-    //         if(obj.get()==&it) continue;//skip self
-    //         for(auto [h1,w1]:it.body){
-    //             for(auto [h2,w2]:(*obj).body){
-    //                 if(h1==h2 && w1==w2){
-    //                     return 1;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return 0;
-    // }
+//detect if it is clash with other object
+//only sometimes it will check. 
+// template<typename Container>
+// auto check_collision(Object it) -> decltype(std::forward<Container>(objs), int()) {
+//     using ValueType = typename std::decay_t<Container>::value_type;
+//     static_assert(
+//         std::is_same_v<ValueType, std::shared_ptr<Object>>,
+//         "Container must hold shared_ptr<Object>"
+//     );
+//     static_assert(
+//         std::is_same_v<std::decay_t<Container>, std::vector<ValueType>>,
+//         "Container must be a vector"
+//     );
+//     for(auto&& obj : objs){
+//         if(obj.get()==&it) continue;//skip self
+//         for(auto [h1,w1]:it.body){
+//             for(auto [h2,w2]:(*obj).body){
+//                 if(h1==h2 && w1==w2){
+//                     return 1;
+//                 }
+//             }
+//         }
+//     }
+//     return 0;
+// }
 
-    //if all =1, return all as vector. if all =0, return first found.
-    int Frame::set_pixels(std::vector<std::tuple<int,int>>& body , std::vector<std::string>& pixels) {
-        int len=body.size();
-        for(int i=0; i<len; i++){
-            auto [h,w]=body[i];
-            set_pixel(h,w,pixels[i]);
-        }
-        return 0;
+//if all =1, return all as vector. if all =0, return first found.
+int Frame::set_pixels(std::vector<std::tuple<int,int>>& body , std::vector<std::string>& pixels) {
+    int len = body.size();
+    for(int i = 0; i < len; i++){
+        auto [h,w] = body[i];
+        set_pixel(h, w, pixels[i]);
     }
+    return 0;
+}
 
-    int Frame::set_pixel(int h, int w, std::string& pixel){
-        if(h<0 || h>=HEIGHT || w<0 || w>=WIDTH) return -1;
-        this->pixels[h][w]=pixel;
-        return 0;
-    }
+int Frame::set_pixel(int h, int w, const std::string& pixel){
+    if(h < 0 || h >= m_height || w < 0 || w >= m_width) return -1;
+    this->m_pixels[h][w] = pixel;
+    return 0;
+}
--- Snake/src/frame.hpp
+++ Snake/src/frame.hpp
@@ -10,16 +10,11 @@
 #include "objects/world.hpp"
 
 //一帧是由若干个图层组成的.不过按渲染顺序来. 逻辑和渲染分离.
-class Frame{
-    int HEIGHT;
-    int WIDTH;
-    int OFFSET_H;
-    int OFFSET_W;
-    std::vector<std::vector<std::string>> pixels;
-private:
-
+class Frame
+{
 public:
-    Frame(int H=30, int W=40, int OH=2, int OW=5);//todo: const
+    Frame(int h = 30, int w = 40, int oh = 2, int ow = 5);//todo: const
+    ~Frame();
 
     bool flush_to_screen(World& world);//world is only one in most cases, so I don't want use xxx_ptr here.
 
@@ -31,5 +26,12 @@ public:
     auto search(int all);
 
     int set_pixels(std::vector<std::tuple<int,int>>& body , std::vector<std::string>& pixel);
-    int set_pixel(int h, int w, std::string& pixel);
+    int set_pixel(int h, int w, const std::string& pixel);
+
+protected:
+    int m_height;
+    int m_width;
+    int m_offset_h;
+    int m_offset_w;
+    std::vector<std::vector<std::string>> m_pixels;
 };
